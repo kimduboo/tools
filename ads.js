94e3fa047b2923109407a1b06aa930b857a534ef
joyful101 <kimduboo@naver.com>
2025-08-10 23:37:54 +0900

    20250810 2337

---
+++ ads.js
@@ -0,0 +1,8 @@
+// ads.js 파일 생성
+(function() {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6022794906780783';
+    script.crossOrigin = 'anonymous';
+    document.head.appendChild(script);
+  })();
